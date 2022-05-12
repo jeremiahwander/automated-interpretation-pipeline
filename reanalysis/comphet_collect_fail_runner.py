fdf2f063e36d496c2921fdbeec418e1e2fb18e1c
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-12 11:16:02 +1000

    Try HAIL_JAR_URL instead

--- reanalysis/comphet_collect_fail_runner.py
+++ reanalysis/comphet_collect_fail_runner.py
@@ -4,15 +4,12 @@ if __name__ == '__main__':
     BILLING_PROJECT = 'test'
     REMOTE_TMPDIR = 'gs://leo-tmp-au/batch-tmp'
 
-    service_backend = hb.ServiceBackend(
-        billing_project=BILLING_PROJECT,
-        remote_tmpdir=REMOTE_TMPDIR,
-        jar_url='gs://hail-test-0d3f214ff5/leonhardgruenschloss/jars/11b4a55862da39b90f895191cc44bd32dd92a046.jar',
-    )
+    service_backend = hb.ServiceBackend(billing_project=BILLING_PROJECT, remote_tmpdir=REMOTE_TMPDIR)
 
     batch = hb.Batch(name='oom-heap-dump', backend=service_backend)
     job = batch.new_job()
     job.image('australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:8f74e06257a75383689bc38ab3e88add6df8462c-hail-bb0dd360e4d546ff948c1d7b43614667e829be57')
+    job.env('HAIL_JAR_URL', 'gs://hail-test-0d3f214ff5/leonhardgruenschloss/jars/11b4a55862da39b90f895191cc44bd32dd92a046.jar')
     job.command('git clone -b comp_het_fail_again_leo https://github.com/populationgenomics/automated-interpretation-pipeline.git')
     job.command('python3 automated-interpretation-pipeline/reanalysis/comphet_collect_fail.py')
     batch.run(wait=False)
