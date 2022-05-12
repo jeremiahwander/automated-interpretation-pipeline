68ab3d4cd84c3b87887ba8dd3816c406efcdc2ff
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-12 10:27:58 +1000

    Standalone batch invocation for dev namespace

--- reanalysis/comphet_collect_fail.py
+++ reanalysis/comphet_collect_fail.py
@@ -1,15 +1,33 @@
+import asyncio
+import hailtop.batch as hb
 import hail as hl
-from cpg_utils.hail_batch import init_batch
 
-init_batch()
+def run():
+    BILLING_PROJECT = 'test'
+    REMOTE_TMPDIR = 'gs://leo-tmp-au/batch-tmp'
 
-mt = hl.read_matrix_table('gs://leo-tmp-au/leo-oom-debug/minimal.mt')
-
-mt = mt.select_cols(
-    hets=hl.agg.group_by(
-        mt.gene_id,
-        hl.agg.filter(mt.is_het, hl.agg.collect(mt.row_key)),
+    asyncio.get_event_loop().run_until_complete(
+        hl.init_batch(
+            default_reference='GRCh38',
+            billing_project=BILLING_PROJECT,
+            remote_tmpdir=REMOTE_TMPDIR,
+        )
     )
-)
 
-mt.write('gs://leo-tmp-au/leo-oom-debug/out.mt', overwrite=True)
+    mt = hl.read_matrix_table('gs://leo-tmp-au/leo-oom-debug/minimal.mt')
+
+    mt = mt.select_cols(
+        hets=hl.agg.group_by(
+            mt.gene_id,
+            hl.agg.filter(mt.is_het, hl.agg.collect(mt.row_key)),
+        )
+    )
+
+    mt.write('gs://leo-tmp-au/leo-oom-debug/out.mt', overwrite=True)
+
+if __name__ == '__main__':
+    batch = hb.Batch()
+    job = batch.new_python_job()
+    job.ball(run)
+    batch.run()
+
