645a1391398c2a5093ab3a727dda600602989473
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-12 10:39:52 +1000

    Use standard driver image

--- reanalysis/comphet_collect_fail_runner.py
+++ reanalysis/comphet_collect_fail_runner.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     batch = hb.Batch(name='oom-heap-dump', backend=service_backend)
     job = batch.new_job()
-    job.image('gcr.io/hail-vdc/python-dill:3.7-slim')
+    job.image('australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:8f74e06257a75383689bc38ab3e88add6df8462c-hail-bb0dd360e4d546ff948c1d7b43614667e829be57')
     job.command('git clone https://github.com/populationgenomics/automated-interpretation-pipeline.git')
     job.command('python3 automated-interpretation-pipeline/reanalysis/comphet_collect_fail.py')
     batch.run(wait=False)
