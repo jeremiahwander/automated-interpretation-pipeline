8f5b3568111ad5675e5b53d47fa562518249df0a
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-12 10:35:24 +1000

    Fix command

--- reanalysis/comphet_collect_fail_runner.py
+++ reanalysis/comphet_collect_fail_runner.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     batch = hb.Batch()
     job = batch.new_job()
     job.image('gcr.io/hail-vdc/python-dill:3.7-slim')
-    job.call('git clone https://github.com/populationgenomics/automated-interpretation-pipeline.git')
-    job.call('python3 automated-interpretation-pipeline/reanalysis/comphet_collect_fail.py')
+    job.command('git clone https://github.com/populationgenomics/automated-interpretation-pipeline.git')
+    job.command('python3 automated-interpretation-pipeline/reanalysis/comphet_collect_fail.py')
     batch.run(wait=False)
 
