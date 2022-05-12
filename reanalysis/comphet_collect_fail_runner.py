ef3713180aadc435e55271906ea9ef48854582f7
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-12 10:37:07 +1000

    Use service backend

--- reanalysis/comphet_collect_fail_runner.py
+++ reanalysis/comphet_collect_fail_runner.py
@@ -1,7 +1,14 @@
 import hailtop.batch as hb
 
 if __name__ == '__main__':
-    batch = hb.Batch()
+    BILLING_PROJECT = 'test'
+    REMOTE_TMPDIR = 'gs://leo-tmp-au/batch-tmp'
+
+    service_backend = hb.ServiceBackend(
+        billing_project=BILLING_PROJECT, remote_tmpdir=REMOTE_TMPDIR
+    )
+
+    batch = hb.Batch(name='oom-heap-dump', backend=service_backend)
     job = batch.new_job()
     job.image('gcr.io/hail-vdc/python-dill:3.7-slim')
     job.command('git clone https://github.com/populationgenomics/automated-interpretation-pipeline.git')
