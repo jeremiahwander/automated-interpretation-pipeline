99d99701e85efceb4bc427f38da61035f0a4b221
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-16 09:52:43 +1000

    Back to dev namespace

--- reanalysis/comphet_collect_fail_runner.py
+++ reanalysis/comphet_collect_fail_runner.py
@@ -1,8 +1,8 @@
 import hailtop.batch as hb
 
 if __name__ == '__main__':
-    #BILLING_PROJECT = 'test'
-    BILLING_PROJECT = 'leonhardgruenschloss-trial'
+    BILLING_PROJECT = 'test'
+    #BILLING_PROJECT = 'leonhardgruenschloss-trial'
     REMOTE_TMPDIR = 'gs://leo-tmp-au/batch-tmp'
 
     service_backend = hb.ServiceBackend(billing_project=BILLING_PROJECT, remote_tmpdir=REMOTE_TMPDIR)
