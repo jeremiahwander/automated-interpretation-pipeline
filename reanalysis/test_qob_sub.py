1085370eeb3a62276b1871410319a695a702b597
Miah Wander <miah@microsoft.com>
2022-10-10 12:05:11 -0700

    hail-az refs

--- reanalysis/test_qob_sub.py
+++ reanalysis/test_qob_sub.py
@@ -1,34 +1,34 @@
-import os
+#import os
 import logging
 import sys
 import cpg_utils.hail_batch
-import hail
-import asyncio
+#import hail
+#import asyncio
 
-from hail.utils.java import Env
+#from hail.utils.java import Env
 
 def main():
 
-    # cpg_utils.hail_batch.init_batch(
-    #     jar_url="hail-az://hailms02batch/query/jars/1078abac8b8e1c14fe7743aa58bc25118b4108de.jar",
-    #     driver_cores=8, 
-    #     driver_memory='highmem')
+    cpg_utils.hail_batch.init_batch(
+        jar_url="hail-az://hailms02batch/query/jars/1078abac8b8e1c14fe7743aa58bc25118b4108de.jar",
+        driver_cores=8, 
+        driver_memory='highmem')
 
-    if Env._hc:
-        print("Hail Context already initialized")
-    else:
-        logging.info("Initializing QoB")
+    # if Env._hc:
+    #     print("Hail Context already initialized")
+    # else:
+    #     logging.info("Initializing QoB")
 
-        batch = asyncio.get_event_loop().run_until_complete(
-            hail.init_batch(
-                billing_project="severalgenomes", 
-                remote_tmpdir="hail-az://sevgen002sa/cpg-severalgenomes-hail",
-                jar_url="hail-az://hailms02batch/query/jars/1078abac8b8e1c14fe7743aa58bc25118b4108de.jar",
-                driver_memory="highmem",
-                driver_cores=8,
-                default_reference="GRCh38"
-            )
-        )
+    #     batch = asyncio.get_event_loop().run_until_complete(
+    #         hail.init_batch(
+    #             billing_project="severalgenomes", 
+    #             remote_tmpdir="hail-az://sevgen002sa/cpg-severalgenomes-hail",
+    #             jar_url="hail-az://hailms02batch/query/jars/1078abac8b8e1c14fe7743aa58bc25118b4108de.jar",
+    #             driver_memory="highmem",
+    #             driver_cores=8,
+    #             default_reference="GRCh38"
+    #         )
+    #     )
 
 
 
