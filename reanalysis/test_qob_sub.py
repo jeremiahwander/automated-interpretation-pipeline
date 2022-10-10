de1585aceed0bca1bb911186c551018a5cde70e7
Miah Wander <miah@microsoft.com>
2022-10-10 10:47:55 -0700

    specifying cloud env var in sub

--- reanalysis/test_qob_sub.py
+++ reanalysis/test_qob_sub.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import sys
 from cpg_utils.hail_batch import init_batch
@@ -5,6 +6,13 @@ from cpg_utils.hail_batch import init_batch
 def main():
 
     logging.info("Initializing QoB")
+
+    if 'HAIL_CLOUD' in os.environ.keys:
+        print(f"HAIL_CLOUD = {os.environ['HAIL_CLOUD']}")
+    else:
+        print("HAIL_CLOUD IS NOT SET.")
+        
+    os.environ['HAIL_CLOUD'] = 'azure'
 
     # initiate Hail with defined driver spec.
     init_batch(driver_cores=8, driver_memory='highmem')
