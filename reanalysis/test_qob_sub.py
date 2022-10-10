4396b15eecbbb12a82cf1ff0e28f450f3355fb98
Miah Wander <miah@microsoft.com>
2022-10-10 11:21:18 -0700

    AB test part A

--- reanalysis/test_qob_sub.py
+++ reanalysis/test_qob_sub.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import sys
-# from cpg_utils.hail_batch import init_batch
+import cpg_utils.hail_batch
 import hail
 import asyncio
 
@@ -9,14 +9,16 @@ from hail.utils.java import Env
 
 def main():
 
-    logging.info("Initializing QoB")
-
-    # # initiate Hail with defined driver spec.
-    # init_batch(driver_cores=8, driver_memory='highmem')
+    # cpg_utils.hail_batch.init_batch(
+    #     jar_url="hail-az://hailms02batch/query/jars/1078abac8b8e1c14fe7743aa58bc25118b4108de.jar",
+    #     driver_cores=8, 
+    #     driver_memory='highmem')
 
     if Env._hc:
         print("Hail Context already initialized")
     else:
+        logging.info("Initializing QoB")
+
         batch = asyncio.get_event_loop().run_until_complete(
             hail.init_batch(
                 billing_project="severalgenomes", 
