84d039dd23de0153cbd19995ac6eee6f6381b190
Miah Wander <miah@microsoft.com>
2022-10-10 11:09:52 -0700

    add jar url

--- reanalysis/test_qob_sub.py
+++ reanalysis/test_qob_sub.py
@@ -13,7 +13,15 @@ def main():
     # init_batch(driver_cores=8, driver_memory='highmem')
 
 
-    batch = asyncio.get_event_loop().run_until_complete(hail.init_batch(billing_project="severalgenomes", remote_tmpdir="hail-az://sevgen002sa/cpg-severalgenomes-hail"))
+    batch = asyncio.get_event_loop().run_until_complete(
+        hail.init_batch(
+            billing_project="severalgenomes", 
+            remote_tmpdir="hail-az://sevgen002sa/cpg-severalgenomes-hail",
+            jar_url="hail-az://hailms02batch/query/jars/1078abac8b8e1c14fe7743aa58bc25118b4108de.jar",
+            driver_memory="highmem",
+            driver_cores=8
+        )
+    )
 
 
 
