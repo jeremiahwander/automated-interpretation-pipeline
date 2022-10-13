cb2ef3fe65adca1606512ae2a5895a72e13834e3
Miah Wander <miah@microsoft.com>
2022-10-13 08:43:18 -0700

    switching runner from main to test blobs

--- reanalysis/run_reanalysis.sh
+++ reanalysis/run_reanalysis.sh
@@ -10,10 +10,10 @@ analysis-runner \
   --description "AIP runtime test" \
   -o "reanalysis/${PAP_DATE}" \
   --access-level test \
-  --env CPG_CONFIG_PATH="hail-az://sevgen002sa/cpg-severalgenomes-main/cpg-config.toml" \
+  --env CPG_CONFIG_PATH="hail-az://sevgen002sa/cpg-severalgenomes-test/cpg-config.toml" \
   reanalysis/interpretation_runner.py \
-    --config_json hail-az://sevgen002sa/cpg-severalgenomes-main/reanalysis/reanalysis_conf.json \
-    --input_path hail-az://sevgen002sa/cpg-severalgenomes-main/reanalysis/986d792a448c66a8a5cfba65434e7d1ce9b1ff_1051-validation.mt \
-    --panel_genes hail-az://sevgen002sa/cpg-severalgenomes-main/reanalysis/pre_panelapp_mendeliome.json \
-    --plink_file hail-az://sevgen002sa/cpg-severalgenomes-main/reanalysis/pedigree.fam \
+    --config_json hail-az://sevgen002sa/cpg-severalgenomes-test/reanalysis/reanalysis_conf.json \
+    --input_path hail-az://sevgen002sa/cpg-severalgenomes-test/reanalysis/986d792a448c66a8a5cfba65434e7d1ce9b1ff_1051-validation.mt \
+    --panel_genes hail-az://sevgen002sa/cpg-severalgenomes-test/reanalysis/pre_panelapp_mendeliome.json \
+    --plink_file hail-az://sevgen002sa/cpg-severalgenomes-test/reanalysis/pedigree.fam \
     --skip_annotation
