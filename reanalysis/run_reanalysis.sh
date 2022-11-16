b4509f7006bdc3c82083aa18a73cf3a8d9f6998d
Miah Wander <miah@microsoft.com>
2022-11-16 21:37:02 +0000

    prep to run on vcf

--- reanalysis/run_reanalysis.sh
+++ reanalysis/run_reanalysis.sh
@@ -5,6 +5,8 @@ set -ex
 # set the date, or provide a default
 DATE=${1:-$(date +%F)}
 
+#     -i hail-az://sevgen002sa/test/reanalysis/986d792a448c66a8a5cfba65434e7d1ce9b1ff_1051-validation.mt \
+
 analysis-runner \
   --dataset severalgenomes \
   --description "AIP run" \
@@ -13,6 +15,5 @@ analysis-runner \
   --config reanalysis/reanalysis_global.toml \
   --config reanalysis/reanalysis_cohort.toml \
   reanalysis/interpretation_runner.py \
-    -i hail-az://sevgen002sa/test/reanalysis/986d792a448c66a8a5cfba65434e7d1ce9b1ff_1051-validation.mt \
-    --pedigree hail-az://sevgen002sa/test/reanalysis/pedigree.fam \
-    --skip_annotation
+    -i hail-az://sevgen002sa/test/reanalysis/2022-11-16/prior_to_annotation.vcf.bgz \
+    --pedigree hail-az://sevgen002sa/test/reanalysis/pedigree.fam
