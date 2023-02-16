ed072a41a3a7c355c188d8b08ef8269f679814ae
Miah Wander <miah@microsoft.com>
2023-02-16 05:21:28 +0000

    updating runners for cpg inputs

--- reanalysis/run_reanalysis_direct.sh
+++ reanalysis/run_reanalysis_direct.sh
@@ -6,7 +6,7 @@ set -ex
 
 # set the date, or provide a default
 DATE=${1:-$(date +%F)}
-#DATE="2023-01-26"
+DATE="2023-02-15"
 # make a randomized config name
 CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
 
@@ -16,7 +16,7 @@ python3 reanalysis/generate_workflow_config.py \
   --dataset rgp \
   --access_level test \
   --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_aip:latest \
-  --output_prefix "reanalysis_train/${DATE}" \
+  --output_prefix "reanalysis_train_test_cpg/${DATE}" \
   --extra_datasets severalgenomes rgp \
   --image_base azcpg001acr.azurecr.io/cpg-common/images \
   --reference_base hail-az://azcpg001sa/reference \
@@ -26,6 +26,6 @@ python3 reanalysis/generate_workflow_config.py \
 
 export CPG_CONFIG_PATH=${CONFIG_PATH}
 python3 reanalysis/interpretation_runner.py \
-  -i hail-az://raregen001sa/test/inputs/rgp/rgp_train_test.vcf.bgz \
-  --pedigree hail-az://raregen001sa/test/inputs/rgp/rgp_train_test.fam \
-  --participant_panels hail-az://raregen001sa/test/inputs/rgp/rgp_hpo_based_panels.json \
+  -i hail-az://raregen001sa/test/inputs/rgp/cpg/3355c9263be6d4b6e13c88b95fb0e3bc1bc99d_1559-broad-rgp.mt \
+  --pedigree hail-az://raregen001sa/test/inputs/rgp/cpg/pedigree.ped \
+  --participant_panels hail-az://raregen001sa/test/inputs/rgp/cpg/participant_panels.json \
