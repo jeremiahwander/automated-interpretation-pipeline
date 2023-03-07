2560a271908365e682931ff879d60600fc08ccf5
Miah Wander <miah@microsoft.com>
2023-03-07 00:46:10 +0000

    trying out specific changes

--- reanalysis/run_reanalysis_direct.sh
+++ reanalysis/run_reanalysis_direct.sh
@@ -8,18 +8,20 @@ set -ex
 #DATE=${1:-$(date +%F)}
 DATE="2023-01-26"
 # make a randomized config name
-CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
+CONFIG_PATH=hail-az://kaileighdemo1sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
 
   # --deploy_config ~/sources/cpg/cpg-deploy/azure/deploy-config.prod.json \
   # --server_config ~/sources/cpg/cpg-deploy/aip/terraform.tfvars.json \
 python3 reanalysis/generate_workflow_config.py \
-  --dataset rgp \
+  --dataset rgptest \
   --access_level test \
-  --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_aip:latest \
+  --driver_image kaileighacr.azurecr.io/cpg-common/images/cpg_aip:latest \
   --output_prefix "reanalysis_train/${DATE}" \
-  --image_base azcpg001acr.azurecr.io/cpg-common/images \
-  --reference_base hail-az://azcpg001sa/reference \
+  --image_base kaileighacr.azurecr.io/cpg-common/images \
+  --reference_base hail-az://kaileighref/reference \
   --extra_configs reanalysis/reanalysis_global.toml reanalysis/reanalysis_cohort.toml \
+  --server_config /home/azureuser/cpg-deploy/aip/terraform.tfvars.json \
+  --deploy_config /home/azureuser/deploy_config.json \
   -o ${CONFIG_PATH}
   
 export CPG_CONFIG_PATH=${CONFIG_PATH}
