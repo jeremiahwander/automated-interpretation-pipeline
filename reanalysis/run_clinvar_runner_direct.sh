4012879306cbeb71f8237d34c1481324c24b1159
Ubuntu <kahlquis@aip-test.ptargitg0icehjpm43ps5tgpbf.bx.internal.cloudapp.net>
2023-05-19 19:36:58 +0000

    localizing for kahlquis and broad azure hail cluster

--- reanalysis/run_clinvar_runner_direct.sh
+++ reanalysis/run_clinvar_runner_direct.sh
@@ -5,21 +5,22 @@ set -ex
 # Make sure to export CPG_DEPLOY_CONFIG first?
 
 # set the date, or provide a default
-DATE=${1:-$(date +%F)}
-#DATE="2023-02-15"
+# DATE=${1:-$(date +%F)}
+DATE="2023-05-19"
 # make a randomized config name
-CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
+CONFIG_PATH=hail-az://kahlquisrefsa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
 
   # --deploy_config ~/sources/cpg/cpg-deploy/azure/deploy-config.prod.json \
   # --server_config ~/sources/cpg/cpg-deploy/aip/terraform.tfvars.json \
 python3 reanalysis/generate_workflow_config.py \
-  --dataset severalgenomes \
+  --dataset rgptest \
   --access_level test \
-  --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_aip:latest \
-  --output_prefix "reanalysis/${DATE}" \
-  --extra_datasets severalgenomes rgp \
-  --image_base azcpg001acr.azurecr.io/cpg-common/images \
-  --reference_base hail-az://azcpg001sa/reference \
+  --driver_image kahlquisaipacr.azurecr.io/cpg-common/images/cpg_aip:latest \
+  --output_prefix "reanalysis_test/${DATE}" \
+  --image_base kahlquisaipacr.azurecr.io/cpg-common/images \
+  --reference_base hail-az://kahlquisref/reference \
+  --server_config /home/kahlquis/cpg-deploy/aip/terraform.tfvars.json \
+  --deploy_config /home/kahlquis/deploy_config.json \
   --extra_configs reanalysis/reanalysis_global.toml reanalysis/reanalysis_cohort.toml \
   -o ${CONFIG_PATH}
 
--- reanalysis/run_reanalysis_direct.sh
+++ reanalysis/run_reanalysis_direct.sh
@@ -5,28 +5,48 @@ set -ex
 # Make sure to export CPG_DEPLOY_CONFIG first?
 
 # set the date, or provide a default
-DATE=${1:-$(date +%F)}
-#DATE="2023-02-15"
+#DATE=${1:-$(date +%F)}
+DATE="2023-05-19"
 # make a randomized config name
-CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
+CONFIG_PATH=hail-az://kahlquisrefsa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
+#CONFIG_PATH=temp.toml
+
 
   # --deploy_config ~/sources/cpg/cpg-deploy/azure/deploy-config.prod.json \
   # --server_config ~/sources/cpg/cpg-deploy/aip/terraform.tfvars.json \
 python3 reanalysis/generate_workflow_config.py \
-  --dataset severalgenomes \
+  --dataset rgptest \
   --access_level test \
-  --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_aip:latest \
-  --output_prefix "reanalysis/${DATE}" \
-  --extra_datasets severalgenomes \
-  --image_base azcpg001acr.azurecr.io/cpg-common/images \
-  --reference_base hail-az://azcpg001sa/reference \
+  --driver_image kahlquisaipcr.azurecr.io/cpg-common/images/cpg_aip:latest \
+  --output_prefix "reanalysis_train/${DATE}" \
+  --image_base kahlquisaipcr.azurecr.io/cpg-common/images \
+  --reference_base hail-az://kahlquisref/reference \
+  --server_config /home/kahlquis/cpg-deploy/aip/terraform.tfvars.json \
+  --deploy_config /home/kahlquis/deploy_config.json \
   --extra_configs reanalysis/reanalysis_global.toml reanalysis/reanalysis_cohort.toml \
   -o ${CONFIG_PATH}
+  
+# export CPG_CONFIG_PATH=${CONFIG_PATH}
+# python3 reanalysis/interpretation_runner.py \
+#   -i hail-az://kahlquisref/test/inputs/prior_to_annotation.vcf.bgz \
+#   --pedigree hail-az://kahlquisref/test/inputs/pedigree.fam
 
+# export CPG_CONFIG_PATH=${CONFIG_PATH}
+# python3 reanalysis/interpretation_runner.py \
+#   -i hail-az://kahlquisref/test/inputs/single_line_test.vcf.gz \
+#   --pedigree hail-az://kahlquisref/test/inputs/single_line_test.ped
 
-#  -i hail-az://raregen001sa/test/inputs/rgp/cpg/3355c9263be6d4b6e13c88b95fb0e3bc1bc99d_1559-broad-rgp.mt \
+# export CPG_CONFIG_PATH=${CONFIG_PATH}
+# python3 reanalysis/interpretation_runner.py \
+#   -i hail-az://kahlquisref/test/inputs/grch38_WAS_plus_seqdict.vcf.gz \
+#   --pedigree hail-az://kahlquisref/test/inputs/grch38_WAS.ped
+
+# export CPG_CONFIG_PATH=${CONFIG_PATH}
+# python3 reanalysis/interpretation_runner.py \
+#   -i hail-az://kahlquisref/test/inputs/grch38_WAS_male01.vcf.gz \
+#   --pedigree hail-az://kahlquisref/test/inputs/grch38_WAS_male.ped
 
 export CPG_CONFIG_PATH=${CONFIG_PATH}
 python3 reanalysis/interpretation_runner.py \
-  -i hail-az://sevgen002sa/test/reanalysis/2022-11-15/prior_to_annotation.vcf.bgz \
-  --pedigree hail-az://sevgen002sa/test/reanalysis/pedigree.fam
+  -i hail-az://kahlquisref/reference/RGP_data/RGP_microsoft_output_111422_train.vcf.bgz \
+  --pedigree hail-az://kahlquisref/reference_RGP_data/train_pedigree.fam
