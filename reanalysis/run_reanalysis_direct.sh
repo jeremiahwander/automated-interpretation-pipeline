2350653b52987deaff99c82a4e79e7c71771f8b1
Ubuntu <kahlquis@aip-test.ptargitg0icehjpm43ps5tgpbf.bx.internal.cloudapp.net>
2023-05-22 19:53:15 +0000

    updates to clinvar process steps

--- reanalysis/run_reanalysis_direct.sh
+++ reanalysis/run_reanalysis_direct.sh
@@ -6,9 +6,9 @@ set -ex
 
 # set the date, or provide a default
 #DATE=${1:-$(date +%F)}
-DATE="2023-05-19"
+DATE="2023-05-22"
 # make a randomized config name
-CONFIG_PATH=hail-az://kahlquisrefsa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
+CONFIG_PATH=https://kahlquisrefsa.blob.core.windows.net/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
 #CONFIG_PATH=temp.toml
 
 
@@ -20,7 +20,7 @@ python3 reanalysis/generate_workflow_config.py \
   --driver_image kahlquisaipcr.azurecr.io/cpg-common/images/cpg_aip:latest \
   --output_prefix "reanalysis_train/${DATE}" \
   --image_base kahlquisaipcr.azurecr.io/cpg-common/images \
-  --reference_base hail-az://kahlquisref/reference \
+  --reference_base https://kahlquisref.blob.core.windows.net/reference \
   --server_config /home/kahlquis/cpg-deploy/aip/terraform.tfvars.json \
   --deploy_config /home/kahlquis/deploy_config.json \
   --extra_configs reanalysis/reanalysis_global.toml reanalysis/reanalysis_cohort.toml \
@@ -48,5 +48,5 @@ python3 reanalysis/generate_workflow_config.py \
 
 export CPG_CONFIG_PATH=${CONFIG_PATH}
 python3 reanalysis/interpretation_runner.py \
-  -i hail-az://kahlquisref/reference/RGP_data/RGP_microsoft_output_111422_train.vcf.bgz \
-  --pedigree hail-az://kahlquisref/reference_RGP_data/train_pedigree.fam
+  -i https://kahlquisref.blob.core.windows.net/reference/RGP_data/RGP_microsoft_output_111422_train.vcf.bgz \
+  --pedigree https://kahlquisref.blob.core.windows.net/reference/RGP_data/train_pedigree.fam
