1735e8fbfe26808fd2a1f4004e34dcc9628060eb
Ubuntu <kahlquis@aip-test.ptargitg0icehjpm43ps5tgpbf.bx.internal.cloudapp.net>
2023-05-22 19:53:15 +0000

    updates to clinvar process steps

--- reanalysis/run_clinvar_runner_direct.sh
+++ reanalysis/run_clinvar_runner_direct.sh
@@ -6,19 +6,20 @@ set -ex
 
 # set the date, or provide a default
 # DATE=${1:-$(date +%F)}
-DATE="2023-05-19"
+DATE="2023-05-22"
 # make a randomized config name
-CONFIG_PATH=hail-az://kahlquisrefsa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
+
+CONFIG_PATH=https://kahlquisrefsa.blob.core.windows.net/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
 
   # --deploy_config ~/sources/cpg/cpg-deploy/azure/deploy-config.prod.json \
   # --server_config ~/sources/cpg/cpg-deploy/aip/terraform.tfvars.json \
 python3 reanalysis/generate_workflow_config.py \
   --dataset rgptest \
   --access_level test \
-  --driver_image kahlquisaipacr.azurecr.io/cpg-common/images/cpg_aip:latest \
+  --driver_image kahlquisaipcr.azurecr.io/cpg-common/images/cpg_aip:latest \
   --output_prefix "reanalysis_test/${DATE}" \
-  --image_base kahlquisaipacr.azurecr.io/cpg-common/images \
-  --reference_base hail-az://kahlquisref/reference \
+  --image_base kahlquisaipcr.azurecr.io/cpg-common/images \
+  --reference_base https://kahlquisref.blob.core.windows.net/reference \
   --server_config /home/kahlquis/cpg-deploy/aip/terraform.tfvars.json \
   --deploy_config /home/kahlquis/deploy_config.json \
   --extra_configs reanalysis/reanalysis_global.toml reanalysis/reanalysis_cohort.toml \
