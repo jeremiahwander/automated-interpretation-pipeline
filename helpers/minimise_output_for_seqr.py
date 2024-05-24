0b3e1e4cec6132905505bde923d7fb57c3e702cc
kdahlo <6993450+kdahlo@users.noreply.github.com>
2024-05-24 19:24:00 +0000

    added category exclusion option to minimise for seqr script

--- helpers/minimise_output_for_seqr.py
+++ helpers/minimise_output_for_seqr.py
@@ -33,7 +33,7 @@ def coord_to_string(coord: dict) -> str:
     return f"{coord['chrom']}-{coord['pos']}-{coord['ref']}-{coord['alt']}"
 
 
-def main(input_file: str, output: str):
+def main(input_file: str, output: str, category_exclude: list):
     """
     reads in the input file, shrinks it, and writes the output file
 
@@ -44,26 +44,32 @@ def main(input_file: str, output: str):
 
     with open(input_file, encoding='utf-8') as f:
         data = ResultData.model_validate(json.load(f))
-
+        cat_dict = data.metadata.categories
+        for key in category_exclude:
+            cat_dict.pop(key, None)
     lil_data = MiniForSeqr(
         **{
-            'metadata': {'categories': data.metadata.categories},
+            'metadata': {'categories': cat_dict},
         }
     )
-
+    l = [[{x},{x,'support'},{'support',x}] for x in category_exclude]
+    exclude = [x for y in l for x in y]
+    #print(exclude)
     for individual, details in data.results.items():
-        lil_data.results[individual] = {}
         for variant in details.variants:
             var_data = variant.var_data
-            lil_data.results[individual][
-                var_data.coordinates.string_format
-            ] = MiniVariant(
-                **{
-                    'categories': variant.categories,
-                    'support_vars': variant.support_vars,
-                    'independent': variant.independent,
-                }
-            )
+            #print(variant.categories)
+            if variant.categories not in exclude:
+                lil_data.results[individual] = {}
+                lil_data.results[individual][
+                    var_data.coordinates.string_format
+                ] = MiniVariant(
+                    **{
+                        'categories': variant.categories-set(category_exclude),
+                        'support_vars': variant.support_vars,
+                        'independent': variant.independent,
+                    }
+                )
 
     with open(output, 'w', encoding='utf-8') as f:
         f.write(MiniForSeqr.model_validate(lil_data).model_dump_json(indent=4))
@@ -73,6 +79,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('input_file', help='the input file to process')
     parser.add_argument('output_file', help='the output file to write to')
+    parser.add_argument('-ex', '--exclude', help='list of categories to exclude', default='')
     args = parser.parse_args()
-
-    main(input_file=args.input_file, output=args.output_file)
+    catex = args.exclude.split(',')
+    main(input_file=args.input_file, output=args.output_file, category_exclude=catex)
